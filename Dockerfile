74e4f1ba2e73a395fa2210382009f83bff02d564
Dipu Sharma <33049521+DipuSharma@users.noreply.github.com>
2021-12-07 19:18:51 +0530

    Commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.9.6
+RUN pip install fastapi uvicorn databases aiosqlite pydantic SQLAlchemy fastapi-mail starlette
+COPY ./app /app
+EXPOSE 80
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,19 +1,50 @@
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Depends, APIRouter, BackgroundTasks, UploadFile, File, Form
+from pydantic import BaseModel, Field, EmailStr
 from typing import List
 import databases
 import sqlalchemy
 from datetime import datetime
+from sqlalchemy import ForeignKey, Boolean, Column, Integer, String
+from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME="sdipu231@gmail.com",  ## Enter Your Gmail Id
+    MAIL_PASSWORD="Kanchan57@",  ## Enter Your Password
+    MAIL_FROM="sdipu231@gmail.com",  ## Enter Again Your Gmail Id
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
+)
+
 tags = [{"name": "Create", "description": "This are my user creation routes"},
         {"name": "Get_All", "description": "This are my all Data fetch routes"},
         {"name": "Get_One", "description": "This are my Single data fetch routes"},
         {"name": "Update", "description": "This are my Single Data Updating routes"},
-        {"name": "Delete", "description": "This are Single Data Deletion routes"}]
+        {"name": "Delete", "description": "This are Single Data Deletion routes"},
+        {"name": "Send-Mail", "description": "Send Mail Using Fastapi routes"}]
+
 app = FastAPI(title='My App',
+              version="0.0.0.1",
               contact={"name": "Dipu Kumar Sharma",
                        "email": "dipu.s@dreamztech.com"},
               openapi_tags=tags,
               docs_url='/dipu')
+
+html = """
+<p>Kya Hall hei Madam </p> 
+<h1>Etna Gussa nhi hoteyin</h1>
+"""
 
 DATABASE_URL = "sqlite:///./Student.db"
 
@@ -25,6 +56,14 @@ register = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("name", sqlalchemy.String(500)),
     sqlalchemy.Column("date_created", sqlalchemy.DateTime())
+
+)
+store = sqlalchemy.Table(
+    "Book",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(500)),
+    sqlalchemy.Column("student_id", sqlalchemy.ForeignKey("Student.id"))
 
 )
 
@@ -102,3 +141,17 @@ async def delete(id: int):
     delete_id = await database.execute(query)
     if not delete_id:
         return {'messages': f"No details found of this Id{id}"}
+
+
+@app.post("/email", tags=['Send-Mail'])
+async def simple_send(email: EmailSchema) -> JSONResponse:
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients=email.dict().get("email"),  # List of recipients, as many as you can pass
+        body=html,
+        subtype="html"
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
